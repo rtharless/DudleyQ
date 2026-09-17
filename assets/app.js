761f931ff7b9e0d99fb22f0df8a57ee137d140d5
Abhishek Dabe <abhishek.dabe@neosoftmail.com>
2026-09-17 15:19:13 +0530

    feedback points from the call

--- assets/app.js
+++ assets/app.js
@@ -4,14 +4,14 @@ document.addEventListener('DOMContentLoaded', function () {
         const plusIcon = document.querySelector('.tab-benefits-plus-icon');
         const minusIcon = document.querySelector('.tab-benefits-minus-icon');
         const tabDetails = document.querySelector('.tab-benefits-detail');
-        if (minusIcon.style.display === 'none') {
-            minusIcon.style.display = 'block';
-            plusIcon.style.display = 'none';
-            tabDetails.style.display = 'block';
-        }else{
-            minusIcon.style.display = 'none';
+        if (plusIcon.style.display === 'none') {
             plusIcon.style.display = 'block';
+            minusIcon.style.display = 'none';
             tabDetails.style.display = 'none';
+        }else{
+            plusIcon.style.display = 'none';
+            minusIcon.style.display = 'block';
+            tabDetails.style.display = 'block';
         }
     });
     const btn2 = document.querySelector('.how_to_use');
@@ -19,14 +19,14 @@ document.addEventListener('DOMContentLoaded', function () {
         const plusIcon = document.querySelector('.tab-howtouse-plus-icon');
         const minusIcon = document.querySelector('.tab-howtouse-minus-icon');
         const tabDetails = document.querySelector('.tab-howtouse-detail');
-        if (minusIcon.style.display === 'none') {
-            minusIcon.style.display = 'block';
-            plusIcon.style.display = 'none';
-            tabDetails.style.display = 'block';
-        }else{
-            minusIcon.style.display = 'none';
+        if (plusIcon.style.display === 'none') {
             plusIcon.style.display = 'block';
+            minusIcon.style.display = 'none';
             tabDetails.style.display = 'none';
+        }else{
+            plusIcon.style.display = 'none';
+            minusIcon.style.display = 'block';
+            tabDetails.style.display = 'block';
         }
     });
     const btn3 = document.querySelector('.key_ingredients');
@@ -34,14 +34,14 @@ document.addEventListener('DOMContentLoaded', function () {
         const plusIcon = document.querySelector('.tab-keying-plus-icon');
         const minusIcon = document.querySelector('.tab-keying-minus-icon');
         const tabDetails = document.querySelector('.tab-keying-detail');
-        if (minusIcon.style.display === 'none') {
-            minusIcon.style.display = 'block';
-            plusIcon.style.display = 'none';
-            tabDetails.style.display = 'block';
-        }else{
-            minusIcon.style.display = 'none';
+        if (plusIcon.style.display === 'none') {
             plusIcon.style.display = 'block';
+            minusIcon.style.display = 'none';
             tabDetails.style.display = 'none';
+        }else{
+            plusIcon.style.display = 'none';
+            minusIcon.style.display = 'block';
+            tabDetails.style.display = 'block';
         }
     });
 });
